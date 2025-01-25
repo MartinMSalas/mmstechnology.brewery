6aecbb2ab6905b9e6bfb7b359df2f7d9744d857a
MartinMSalas <ingmartinmsalas@gmail.com>
2025-01-25 19:57:31 -0300

    feat(BeerDto): create model

---
+++ api-brewery/src/main/java/com/mmstechnology/brewery/api_brewery/ApiBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.mmstechnology.brewery.api_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBreweryApplication.class, args);
+	}
+
+}
---
+++ api-brewery/src/test/java/com/mmstechnology/brewery/api_brewery/ApiBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmstechnology.brewery.api_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
